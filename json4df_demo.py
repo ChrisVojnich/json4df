e11a1a53e8043f88a047c69c510a08ebe43350cc
ChrisVojnich <86104247+ChrisVojnich@users.noreply.github.com>
2021-06-20 10:26:08 +0200

    Add files via upload

---
+++ json4df_demo.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from json2xml import json2xml
+from json2xml.utils import readfromjson,readfromstring
+import lxml
+import lxml.etree as ET
+
+json='example.json'
+xsl='template.xsl'
+filename='json4df_result.json'
+
+
+def json4df (json, xsl, filename):
+    
+    data = readfromjson(json)
+    xml_data=json2xml.Json2xml(data).to_xml()
+    parser = ET.HTMLParser(recover=True)
+    dom = ET.fromstring(xml_data, parser)
+    xslt = ET.parse(xsl)
+    transform = ET.XSLT(xslt)
+    newdom = transform(dom)
+    et=ET.tostring(newdom)
+    df=pd.read_xml(et)
+    df.to_json(filename)
+    return df
+
+
+json4df(json,xsl,filename)
